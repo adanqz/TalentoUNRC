40e2e9fcb228cecf0ee9630e6f98fbcfb8c42959
Adán Quintero <adanqz@gmail.com>
2025-10-27 19:44:01 +0000

    el panel de admnistración no está funcionando en n micrositio independie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
-import Header from '@/components/layout/header';
-import Footer from '@/components/layout/footer';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,11 +23,7 @@ export default function RootLayout({
   return (
     <html lang="es-MX" suppressHydrationWarning>
       <body className={cn("font-body antialiased", inter.variable)}>
-        <div className="flex min-h-screen w-full flex-col">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
+        {children}
         <Toaster />
         </body>
     </html>
