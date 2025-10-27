6b011eb7050df9a97e47203511a6aeaa96bec2a3
Adán Quintero <adanqz@gmail.com>
2025-10-27 14:24:13 +0000

    put a symbol or logo instead text

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Link2Icon className="h-6 w-6 text-primary" />
-      <h1 className="font-headline text-xl font-bold text-primary">
-        API TalentosUNRC
-      </h1>
+      <Link2Icon className="h-8 w-8 text-primary" />
     </Link>
   );
 }
